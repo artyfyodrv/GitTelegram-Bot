9bef89f8d700f07d99a891100f84245177b5db7b
artyfyodrv <artyfyodrv@gmail.com>
2024-01-24 05:48:54 +0300

    #1 Added GithubService at DI

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Services\GithubService;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +13,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(GithubService::class, function ($app) {
+            return new GithubService(new Http(), env('GITHUB_TOKEN'), env('GITHUB_REDIRECT_URL'));
+        });
     }
 
     /**
