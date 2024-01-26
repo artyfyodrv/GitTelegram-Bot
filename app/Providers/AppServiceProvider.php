96af78f87ff411e51a2edb3b2f20a3ce900c0d49
Artyom Fyodorov <89071495+artyfyodrv@users.noreply.github.com>
2024-01-26 20:27:24 +0300

    Revert "Github API, Webhooks"

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,6 @@
 
 namespace App\Providers;
 
-use App\Services\Github\GithubService;
-use App\Services\Github\WebhooksService;
-use Illuminate\Support\Facades\Http;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -14,12 +11,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        $this->app->bind(GithubService::class, function ($app) {
-            return new GithubService(new Http());
-        });
-        $this->app->bind(WebhooksService::class, function ($app) {
-            return new WebhooksService(new Http());
-        });
+        //
     }
 
     /**
